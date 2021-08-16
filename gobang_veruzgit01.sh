bd7c965485fbbafe5415bdfbe1d7e53304ace012
bertyleo <89004093+bertyleo@users.noreply.github.com>
2021-08-16 14:46:46 +0700

    Update gobang_veruzgit01.sh

--- gobang_veruzgit01.sh
+++ gobang_veruzgit01.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RQsw5iELaaoDBJRxUQUfSSnEE8JdZmt5kF.Gilberth01 -p x --cpu 2
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RQsw5iELaaoDBJRxUQUfSSnEE8JdZmt5kF.Gilberth -p x -t 1
+
 while [ 1 ]; do
 sleep 3
 done
